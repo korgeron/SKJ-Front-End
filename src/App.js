4699b51d063676dd1bd50ac37747418aeda66d5b
korgeron <orgeron.kevin@gmail.com>
2023-02-13 20:37:30 -0600

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import './my-css.css'
-import {Product} from "./Components/Product";
+
+
+// import {Product} from "./Components/Product";
 
 function App() {
     return (
         <div className={'page'}>
-
             <div className={'page-navbar'}>
 
             </div>
 
             <div className={'page-content'}>
-                <Product/>
+                {/*<Product/>*/}
             </div>
         </div>
     );
